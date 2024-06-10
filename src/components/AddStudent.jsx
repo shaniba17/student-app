62b3f96524b582f60d9d9aad27f743665a71f719
shaniba17 <shanibatm2002@gmail.com>
2024-06-10 21:34:42 +0530

    add search delete added

---
+++ src/components/AddStudent.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+
+const AddStudent = () => {
+  return (
+    <div>
+        <div className="container">
+            <div className="row">
+                <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                    <div className="row g-3">
+                        <h1>
+                            <u>
+                                <center>Student Details</center>
+                            </u>
+                        </h1>
+                        <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xl6">
+                            <label htmlFor="" className="form-label">Name</label>
+                            <input type="text" className="form-control" />
+                        </div>
+                        <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xl6">
+                            <label htmlFor="" className="form-label">Roll No</label>
+                            <input type="text" className="form-control" />
+                        </div>
+                        <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xl6">
+                            <label htmlFor="" className="form-label">Admission No</label>
+                            <input type="text" className="form-control" />
+                        </div>
+                        <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xl6">
+                            <label htmlFor="" className="form-label">College</label>
+                            <input type="text" className="form-control" />
+                        </div>
+                        <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xl6">
+                            <button className="btn btn-success">Add</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default AddStudent
